b73c7e3a6e706096039906c8bb0bde13c53aa3e5
hlushak <75501308+MarynaHl@users.noreply.github.com>
2022-10-20 20:04:06 +0100

    add styles and API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import './index.css';
 
-import App from "./components/App/App";
-
-import "./index.css";
-
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
+export default function fetchImagesWithQuery(searchQuery, page) {
+  const response = axios.get(
+    `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=29355896-24b645639f363894e45fb57cb&image_type=photo&orientation=horizontal&per_page=12`
+  );
+  return response;
+}
+
+fetchImagesWithQuery.propTypes = {
+  searchQuery: PropTypes.string.isRequired,
+  page: PropTypes.number.isRequired,
+};
